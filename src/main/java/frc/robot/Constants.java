32cb197e711b35018d6599477e9044aca0149e9c
Nick Avtges <navtges@gmail.com>
2022-01-26 18:43:21 -0500

    add cancoder

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -79,4 +79,5 @@ public final class Constants {
 
 	// Turret
 	public static final int TURRET_MOTOR_ID = 0;
+    public static final int TURRET_ENCODER_ID = 0;
 }
--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -4,17 +4,42 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
+import com.ctre.phoenix.sensors.CANCoder;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Turret extends SubsystemBase {
   private TalonFX m_turret;
+  private CANCoder m_encoder;
+
 
   /** Creates a new Turret. */
   public Turret() {
     m_turret = new TalonFX(Constants.TURRET_MOTOR_ID);
+    m_encoder = new CANCoder(Constants.TURRET_ENCODER_ID);
+
+    TalonFXConfiguration config = new TalonFXConfiguration();
+    config.primaryPID.selectedFeedbackSensor = FeedbackDevice.RemoteSensor0;
+    config.peakOutputForward = 1.0;
+    config.peakOutputReverse = -1.0;
+    config.nominalOutputForward = 0;
+    config.nominalOutputReverse = 0;
+    config.neutralDeadband = 0.001;
+    // config.slot0.kP = 0.00000;
+    // config.slot0.kI = 0.00000;
+    // config.slot0.kD = 0.00000;
+    // config.slot0.kF = 1.00000;
+
+    m_turret.configAllSettings(config);
+}
+
+  public void rawMotor(double percentOutput) {
+    m_turret.set(TalonFXControlMode.PercentOutput, percentOutput);
   }
 
   @Override
