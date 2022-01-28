82808fea6710d3f91f5e3f8c649504f790945ba5
pixelated-being <ajd0929@gmail.com>
2022-01-27 19:32:53 -0500

    Add Feeder Commands

---
+++ src/main/java/frc/robot/commands/feeder/Reverse.java
@@ -0,0 +1,25 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.feeder;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.subsystems.Feeder;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class Reverse extends InstantCommand {
+  private Feeder m_feeder;
+  /** Creates a new CentralizerAcceptCargo. */
+  public Reverse(Feeder feeder) {
+    m_feeder = feeder;
+    addRequirements(m_feeder);
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+}
