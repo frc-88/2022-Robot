b864ead24266f5d21deca1bc80eb55bbcfc2f21b
Nick Avtges <navtges@gmail.com>
2022-02-17 20:12:45 -0500

    fix turret calculations

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -93,8 +93,7 @@ public final class Constants {
 	public static final int TURRET_CANCODER_ID = 13;
 	public static final double TURRET_GEAR_RATIO = 7.0 * 144.0 / 18;  // 56.0
 	public static final double TURRET_CANCODER_GEAR_RATIO = 7.0 * 2.0 * 54.0 / 8.0;  // 94.5
-	public static final double TURRET_CANCODER_CONV = TURRET_GEAR_RATIO / TURRET_CANCODER_GEAR_RATIO;
-    public static final double TURRET_COUNTS_PER_REV = 10000;
+    public static final double TURRET_COUNTS_PER_REV = TURRET_GEAR_RATIO * 1024.0;
 	public static final double TURRET_SYNCRONIZATION_THRESHOLD = 1.0;
 	public static final double TURRET_SPIN_THRESHOLD = TURRET_COUNTS_PER_REV / 5.0;
 	public static final double TURRET_ZERO_DFT = 0;
