f25b7f675754d8f01506e75398624fb33d934c46
Nick Avtges <navtges@gmail.com>
2022-03-09 08:58:52 -0500

    different triangles and circle...the limelight moved!

--- src/main/java/frc/robot/util/sensors/Limelight.java
+++ src/main/java/frc/robot/util/sensors/Limelight.java
@@ -117,12 +117,10 @@ public class Limelight {
     }
 
     public double calcTurretOffset() {
-        // double distance = calcDistanceToTarget();
-        // double angle = Math.toRadians(getTargetHorizontalOffsetAngle());
+        double distance = calcDistanceToTarget();
+        double angle = Math.toRadians(getTargetHorizontalOffsetAngle());
 
-        // return hasTarget() ? Math.toDegrees(Math.atan((distance * Math.sin(angle)) /
-        //     (distance * Math.cos(angle) - (isHoodUp()?p_radiusHoodUp.getValue():p_radiusHoodDown.getValue())))) : 0.0;
-        return getTargetHorizontalOffsetAngle();
+        return hasTarget() ? Math.toDegrees(Math.atan(Math.sin(angle)/(Math.cos(angle) + (p_radius.getValue()/distance)))) : 0.0;
     }
 
     /**
