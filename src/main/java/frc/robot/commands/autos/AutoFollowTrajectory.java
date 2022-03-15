97c9b2f3beb49c9ce1b422bda22e27c89472ec38
Nick Avtges <navtges@gmail.com>
2022-03-15 10:25:22 -0400

    move trajectory commands to autos package

--- src/main/java/frc/robot/commands/autos/AutoFollowTrajectory.java
+++ src/main/java/frc/robot/commands/autos/AutoFollowTrajectory.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands.drive;
+package frc.robot.commands.autos;
 
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.math.controller.RamseteController;
