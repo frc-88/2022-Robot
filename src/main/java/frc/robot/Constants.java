c023951128cefb79656ab1effc206e503ed95e5a
Paul Terrasi <paul.a.terrasi@gmail.com>
2022-04-02 10:40:01 -0400

    Fix drive ratios

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -39,8 +39,8 @@ public final class Constants {
 	// Drive Configuration
 	public static final int NUM_DRIVE_MOTORS_PER_SIDE = 2;
 	public static final double WHEEL_DIAMETER = 5.84375;
-	public static final double LOW_GEAR_RATIO = 7.73;
-	public static final double HIGH_GEAR_RATIO = 16.71;
+	public static final double LOW_GEAR_RATIO = 16.71;
+	public static final double HIGH_GEAR_RATIO = 7.73;
 	public static final double LOW_DRIVE_RATIO = (1./LOW_GEAR_RATIO) * (WHEEL_DIAMETER / 12.) * Math.PI;
 	public static final double HIGH_DRIVE_RATIO = (1./HIGH_GEAR_RATIO) * (WHEEL_DIAMETER / 12.) * Math.PI;
 	public static final double DRIVE_SENSOR_RATIO = (1. / ((WHEEL_DIAMETER / 12.) * Math.PI)) * 60./24.;
