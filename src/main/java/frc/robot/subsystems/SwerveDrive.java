e836e35ccbe7a3f149aab09e54bb71e4495d2a77
Nick Avtges <navtges@gmail.com>
2022-10-07 14:32:46 -0400

    adding odometry

--- src/main/java/frc/robot/subsystems/SwerveDrive.java
+++ src/main/java/frc/robot/subsystems/SwerveDrive.java
@@ -14,7 +14,9 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
@@ -27,36 +29,28 @@ import static frc.robot.Constants.*;
 public class SwerveDrive extends SubsystemBase implements ChassisInterface {
         /**
          * The maximum voltage that will be delivered to the drive motors.
-         * <p>
+         * 
          * This can be reduced to cap the robot's maximum speed. Typically, this is
          * useful during initial testing of the robot.
          */
         public static final double MAX_VOLTAGE = 12.0;
-        // TODO Measure the drivetrain's maximum velocity or calculate the theoretical.
-        // The formula for calculating the theoretical maximum velocity is:
-        // <Motor free speed RPM> / 60 * <Drive reduction> * <Wheel diameter meters> *
-        // pi
-        // By default this value is setup for a Mk3 standard module using Falcon500s to
-        // drive.
-        // An example of this constant for a Mk4 L2 module with NEOs to drive is:
-        // 5880.0 / 60.0 / SdsModuleConfigurations.MK4_L2.getDriveReduction() *
-        // SdsModuleConfigurations.MK4_L2.getWheelDiameter() * Math.PI
+
         /**
          * The maximum velocity of the robot in meters per second.
-         * <p>
+         * 
          * This is a measure of how fast the robot should be able to drive in a straight
-         * line.
+         * line. The calculated theoritical maximum is used below.
          */
         public static final double MAX_VELOCITY_METERS_PER_SECOND = 6380.0 / 60.0 *
                         SdsModuleConfigurations.MK4I_L2.getDriveReduction() *
                         SdsModuleConfigurations.MK4I_L2.getWheelDiameter() * Math.PI;
+
         /**
          * The maximum angular velocity of the robot in radians per second.
-         * <p>
+         * 
          * This is a measure of how fast the robot can rotate in place.
+         * The calculated theoritical maximum is used below.
          */
-        // Here we calculate the theoretical maximum angular velocity. You can also
-        // replace this with a measured amount.
         public static final double MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND = MAX_VELOCITY_METERS_PER_SECOND /
                         Math.hypot(DRIVETRAIN_TRACKWIDTH_METERS / 2.0, DRIVETRAIN_WHEELBASE_METERS / 2.0);
 
@@ -70,68 +64,43 @@ public class SwerveDrive extends SubsystemBase implements ChassisInterface {
                         // Back right
                         new Translation2d(-DRIVETRAIN_TRACKWIDTH_METERS / 2.0, -DRIVETRAIN_WHEELBASE_METERS / 2.0));
 
-        // By default we use a Pigeon for our gyroscope. But if you use another
-        // gyroscope, like a NavX, you can change this.
-        // The important thing about how you configure your gyroscope is that rotating
-        // the robot counter-clockwise should
-        // cause the angle reading to increase until it wraps back over to zero.
         private final AHRS m_navx;
-
-        // These are our modules. We initialize them in the constructor.
         private final SwerveModule m_frontLeftModule;
         private final SwerveModule m_frontRightModule;
         private final SwerveModule m_backLeftModule;
         private final SwerveModule m_backRightModule;
 
         private ChassisSpeeds m_chassisSpeeds = new ChassisSpeeds(0.0, 0.0, 0.0);
+        private SwerveDriveOdometry m_odometry;
+        private Pose2d m_pose;
 
         public SwerveDrive(AHRS navx) {
                 m_navx = navx;
                 ShuffleboardTab tab = Shuffleboard.getTab("Drivetrain");
 
                 m_frontLeftModule = Mk4iSwerveModuleHelper.createFalcon500(
-                                // This parameter is optional, but will allow you to see the current state of
-                                // the module on the dashboard.
-                                tab.getLayout("Front Left Module", BuiltInLayouts.kList)
-                                                .withSize(2, 4)
-                                                .withPosition(0, 0),
-                                // This can either be STANDARD or FAST depending on your gear configuration
+                                tab.getLayout("Front Left", BuiltInLayouts.kList).withSize(2, 4).withPosition(0, 0),
                                 Mk4iSwerveModuleHelper.GearRatio.L2,
-                                // This is the ID of the drive motor
                                 FRONT_LEFT_MODULE_DRIVE_MOTOR,
-                                // This is the ID of the steer motor
                                 FRONT_LEFT_MODULE_STEER_MOTOR,
-                                // This is the ID of the steer encoder
                                 FRONT_LEFT_MODULE_STEER_ENCODER,
-                                // This is how much the steer encoder is offset from true zero (In our case,
-                                // zero is facing straight forward)
                                 FRONT_LEFT_MODULE_STEER_OFFSET);
-
-                // We will do the same for the other modules
                 m_frontRightModule = Mk4iSwerveModuleHelper.createFalcon500(
-                                tab.getLayout("Front Right Module", BuiltInLayouts.kList)
-                                                .withSize(2, 4)
-                                                .withPosition(2, 0),
+                                tab.getLayout("Front Right", BuiltInLayouts.kList).withSize(2, 4).withPosition(2, 0),
                                 Mk4iSwerveModuleHelper.GearRatio.L2,
                                 FRONT_RIGHT_MODULE_DRIVE_MOTOR,
                                 FRONT_RIGHT_MODULE_STEER_MOTOR,
                                 FRONT_RIGHT_MODULE_STEER_ENCODER,
                                 FRONT_RIGHT_MODULE_STEER_OFFSET);
-
                 m_backLeftModule = Mk4iSwerveModuleHelper.createFalcon500(
-                                tab.getLayout("Back Left Module", BuiltInLayouts.kList)
-                                                .withSize(2, 4)
-                                                .withPosition(4, 0),
+                                tab.getLayout("Back Left", BuiltInLayouts.kList).withSize(2, 4).withPosition(4, 0),
                                 Mk4iSwerveModuleHelper.GearRatio.L2,
                                 BACK_LEFT_MODULE_DRIVE_MOTOR,
                                 BACK_LEFT_MODULE_STEER_MOTOR,
                                 BACK_LEFT_MODULE_STEER_ENCODER,
                                 BACK_LEFT_MODULE_STEER_OFFSET);
-
                 m_backRightModule = Mk4iSwerveModuleHelper.createFalcon500(
-                                tab.getLayout("Back Right Module", BuiltInLayouts.kList)
-                                                .withSize(2, 4)
-                                                .withPosition(6, 0),
+                                tab.getLayout("Back Right", BuiltInLayouts.kList).withSize(2, 4).withPosition(6, 0),
                                 Mk4iSwerveModuleHelper.GearRatio.L2,
                                 BACK_RIGHT_MODULE_DRIVE_MOTOR,
                                 BACK_RIGHT_MODULE_STEER_MOTOR,
@@ -139,6 +108,9 @@ public class SwerveDrive extends SubsystemBase implements ChassisInterface {
                                 BACK_RIGHT_MODULE_STEER_OFFSET);
 
                 zeroGyroscope();
+
+                m_pose = new Pose2d(Units.feetToMeters(0.0), Units.feetToMeters(0.0), new Rotation2d());
+                m_odometry = new SwerveDriveOdometry(m_kinematics, getGyroscopeRotation(), m_pose);
         }
 
         /**
@@ -161,6 +133,37 @@ public class SwerveDrive extends SubsystemBase implements ChassisInterface {
                 return Rotation2d.fromDegrees(360.0 - m_navx.getYaw());
         }
 
+        public void zeroOdometry() {
+                resetOdometry(new Pose2d(Units.feetToMeters(0.0), Units.feetToMeters(0.0), new Rotation2d()),
+                                new Rotation2d());
+        }
+
+        public void resetOdometry(Pose2d startPose, Rotation2d startGyro) {
+                m_odometry.resetPosition(startPose, startGyro);
+        }
+
+        public void updateOdometry() {
+                m_pose = m_odometry.update(getGyroscopeRotation(),
+                                m_kinematics.toSwerveModuleStates(getChassisVelocity()));
+        }
+
+        public Pose2d getOdometryPose() {
+                return m_pose;
+        }
+
+        public ChassisSpeeds getChassisVelocity() {
+                var frontLeftState = new SwerveModuleState(m_frontLeftModule.getDriveVelocity(),
+                                Rotation2d.fromDegrees(m_frontLeftModule.getSteerAngle()));
+                var frontRightState = new SwerveModuleState(m_frontRightModule.getDriveVelocity(),
+                                Rotation2d.fromDegrees(m_frontRightModule.getSteerAngle()));
+                var backLeftState = new SwerveModuleState(m_backLeftModule.getDriveVelocity(),
+                                Rotation2d.fromDegrees(m_backLeftModule.getSteerAngle()));
+                var backRightState = new SwerveModuleState(m_backRightModule.getDriveVelocity(),
+                                Rotation2d.fromDegrees(m_backRightModule.getSteerAngle()));
+
+                return m_kinematics.toChassisSpeeds(frontLeftState, frontRightState, backLeftState, backRightState);
+        }
+
         public double getAccelerationEstimate() {
                 // TODO
                 return 0.0;
@@ -171,19 +174,6 @@ public class SwerveDrive extends SubsystemBase implements ChassisInterface {
                 return 0.0;
         }
 
-        public void updateOdometry() {
-                // TODO
-        }
-
-        // TODO
-        public void drive(VelocityCommand command) {
-                // TODO? ?
-        }
-
-        public void drive(double vx, double vy, double angularVelocity) {
-                // TODO? ?
-        }
-
         public void stop() {
                 // TODO? ?
         }
@@ -191,17 +181,6 @@ public class SwerveDrive extends SubsystemBase implements ChassisInterface {
         public void resetPosition(Pose2d pose) {
                 // TODO? ?
         }
-
-        public Pose2d getOdometryPose() {
-                // TODO? ?
-                return null;
-        }
-
-        public ChassisSpeeds getChassisVelocity() {
-                // TODO? ?
-                return null;
-        }
-
 
         public void enableMolassesMode() {
                 // TODO? ?
@@ -211,12 +190,23 @@ public class SwerveDrive extends SubsystemBase implements ChassisInterface {
                 // TODO? ?
         }
 
+        public void drive(VelocityCommand command) {
+                m_chassisSpeeds = new ChassisSpeeds(command.vx, command.vy, command.vt);
+        }
+
+        public void drive(double vx, double vy, double angularVelocity) {
+                m_chassisSpeeds = new ChassisSpeeds(vx, vy, angularVelocity);
+        }
+
         public void drive(ChassisSpeeds chassisSpeeds) {
                 m_chassisSpeeds = chassisSpeeds;
         }
 
         @Override
         public void periodic() {
+                updateOdometry();
+
+                // set modules to desired chassis speed
                 SwerveModuleState[] states = m_kinematics.toSwerveModuleStates(m_chassisSpeeds);
                 SwerveDriveKinematics.desaturateWheelSpeeds(states, MAX_VELOCITY_METERS_PER_SECOND);
 
